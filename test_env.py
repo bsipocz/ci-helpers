c67b6795d2df259e9ef80c3f27c5117527a04948
Brigitta Sipocz <bsipocz@gmail.com>
2019-03-17 17:23:37 -0700

    debug

--- test_env.py
+++ test_env.py
@@ -18,6 +18,7 @@ if not LooseVersion(sys.version) < '3.7':
                                      capture_output=True,
                                      text=True).stdout.split()[1]
     if CONDA_VERSION is not None and CONDA_VERSION != 'stable':
+        print("co", CONDA_VERSION, "in", installed_conda)
         assert LooseVersion(CONDA_VERSION) == LooseVersion(installed_conda)
 
 
